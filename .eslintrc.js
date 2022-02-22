36541e82e25f895f0af7f81763ffbaefc3b675c2
Konrad Fedorczyk <contact@realhe.ro>
2022-02-22 11:50:03 +0100

    Update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,16 @@ module.exports = {
     quotes: ["warn", "double", { allowTemplateLiterals: true }],
     "quote-props": ["error", "as-needed"],
     "import/prefer-default-export": "off",
-    "arrow-body-style": "off"
+    "arrow-body-style": "off",
+    "import/extensions": [
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
   },
   extends: ["gatsby-standard", "airbnb-base", "react-app", "prettier"],
 };
