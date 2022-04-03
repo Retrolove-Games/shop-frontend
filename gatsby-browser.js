6c84d388710ce67197b99de5a69694c0b7b89d34
Konrad Fedorczyk <fedek6@gmail.com>
2022-04-03 14:27:37 +0200

    Add CSS reset

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "./src/context/ThemeContext";
+import "@retrolove-games/css-reset";
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
