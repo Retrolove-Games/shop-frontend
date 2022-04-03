7581690c245368cbf8fb93e026b5deab74c067ed
Konrad Fedorczyk <fedek6@gmail.com>
2022-04-03 14:33:15 +0200

    Update component prieview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { LogoDesktop, LogoMobile } from "@retrolove-games/ui-logo";
 import { ThemeContext } from "../context/ThemeContext";
 import { IconMoon, IconSun } from "@retrolove-games/ui-icon";
 import { Switch } from "@retrolove-games/ui-switch";
+import { Lozenge } from "@retrolove-games/ui-lozenge";
 
 // markup
 const IndexPage = () => {
@@ -21,32 +22,56 @@ const IndexPage = () => {
             padding: "100px",
           }}
         >
-          <LogoDesktop />
-          <LogoMobile />
+          <div>
+            <LogoDesktop />
+          </div>
+          <div>
+            <LogoMobile />
+          </div>
 
-          <Switch
-            id="theme"
-            isOn={theme.colorMode === "dark"}
-            leftIcon={<IconSun size="xsmall" />}
-            rightIcon={<IconMoon size="xsmall" />}
-            handleToggle={() =>
-              theme.colorMode === "dark"
-                ? theme.setColorMode("light")
-                : theme.setColorMode("dark")
-            }
-          />
+          <div>
+            <IconSun size={"medium"} />
+          </div>
 
+          <div>
+            <Switch
+              id="theme"
+              isOn={theme.colorMode !== "dark"}
+              leftIcon={<IconSun size="xsmall" />}
+              rightIcon={<IconMoon size="xsmall" />}
+              label="Disable dark mode"
+              handleToggle={() =>
+                theme.colorMode === "dark"
+                  ? theme.setColorMode("light")
+                  : theme.setColorMode("dark")
+              }
+            />
+          </div>
 
-          <Button
-            onClick={() =>
-              theme.colorMode === "dark"
-                ? theme.setColorMode("light")
-                : theme.setColorMode("dark")
-            }
-          >
-            Test
-          </Button>
-          {/* <button onClick={() => theme.colorMode === "dark" ? theme.setColorMode("light") : theme.setColorMode("dark")}>AAAA</button> */}
+          <div>
+            <Button
+              onClick={() =>
+                theme.colorMode === "dark"
+                  ? theme.setColorMode("light")
+                  : theme.setColorMode("dark")
+              }
+            >
+              Test
+            </Button>
+          </div>
+
+          <div>
+            <Lozenge color={"red"}>200 PLN</Lozenge>
+          </div>
+
+          <div>
+            <Button color="red">Static color red</Button>
+          </div>
+
+          <div>
+            <Button color="neonBlue">Static color blue</Button>
+          </div>
+
         </main>
       )}
     </ThemeContext.Consumer>
