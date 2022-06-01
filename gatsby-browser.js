28fe42fe1c0eaffb088e400dbc6dbd005fd7244a
Konrad Fedorczyk <fedek6@gmail.com>
2022-06-01 15:08:31 +0200

    Fix apollo problems

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,18 +2,15 @@ import React from "react";
 import { ThemeProvider } from "./src/store/ThemeContext";
 import { AppStateProvider } from "./src/store/AppStateContext";
 import { SidebarLayout } from "./src/components/Layout";
-import { ApolloClientProvider } from "./src/apollo/ApolloClientProvider";
 import "@retrolove-games/css-reset";
 import "./src/styles/global.css";
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>
     <AppStateProvider>
-      <ApolloClientProvider>
         <div>
           <SidebarLayout>{element}</SidebarLayout>
         </div>
-      </ApolloClientProvider>
     </AppStateProvider>
   </ThemeProvider>
 );
