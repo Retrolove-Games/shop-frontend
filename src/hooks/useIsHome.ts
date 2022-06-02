0bd9aed0507fb62c5691fdd28a80cef76295defb
Konrad Fedorczyk <fedek6@gmail.com>
2022-06-02 16:23:26 +0200

    Update comment in useIsHome

--- src/hooks/useIsHome.ts
+++ src/hooks/useIsHome.ts
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 
+
 export const useIsHome = () => {
   const [isHome, setIsHome] = useState<boolean>();
 
+  // ⛔ Note! This component must use effect!
   useEffect(() => {
     const location = window ? window.location.pathname : null;
 
