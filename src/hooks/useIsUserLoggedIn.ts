1fdd30cfac84f46dc5dbe3d68cd9d3e35896a3ff
Konrad Fedorczyk <fedek6@gmail.com>
2022-06-03 13:28:05 +0200

    Fix formatting

--- src/hooks/useIsUserLoggedIn.ts
+++ src/hooks/useIsUserLoggedIn.ts
@@ -1,11 +1,10 @@
 import { useQuery } from "@apollo/client";
 import { GET_USER_INFO, GetUserInfo } from "@src/queries/userInfo";
 
-
 export const useIsUserLoggedIn = () => {
   const { data, refetch } = useQuery<GetUserInfo>(GET_USER_INFO, {
-    fetchPolicy: 'network-only',
-    nextFetchPolicy: 'cache-first',
+    fetchPolicy: "network-only",
+    nextFetchPolicy: "cache-first",
   });
 
   return {
