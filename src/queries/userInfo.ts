e19e90a07ae7f2966cd036ce6804debfdf641fad
Konrad Fedorczyk <fedek6@gmail.com>
2022-06-03 13:26:45 +0200

    Create a type for userInfo query

--- src/queries/userInfo.ts
+++ src/queries/userInfo.ts
@@ -1,4 +1,10 @@
 import { gql } from "@apollo/client";
+import { Maybe } from "graphql/jsutils/Maybe";
+import { User } from "@src/generated/graphql";
+
+export type GetUserInfo = {
+  viewer: Maybe<Pick<User, "id" | "username" | "firstName" | "lastName">>
+}
 
 export const GET_USER_INFO = gql`
   query UserInfo {
