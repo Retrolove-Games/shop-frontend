84701095c3fe518e408f7b9db20fa6a3b3207269
Konrad Fedorczyk <fedek6@gmail.com>
2022-06-04 12:23:29 +0200

    Add compact product component

---
+++ src/components/Product/CompactProduct/CompactProduct.styles.ts
@@ -0,0 +1,13 @@
+import { styled, darkTheme } from "@retrolove-games/ui-themes";
+
+export const Wrapper = styled("div", {
+  /**
+   * Themes
+   */
+  [`.${darkTheme} &`]: {},
+
+  /**
+   * Variants
+   */
+  variants: {},
+});
